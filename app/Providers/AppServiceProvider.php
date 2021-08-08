ca073c82c863d8107c151047887b354ea823336d
minhvinhho <61456244+minhvinhho@users.noreply.github.com>
2021-08-08 13:12:50 +0700

    update Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,9 @@ use App\Models\Category;
 use App\Models\Config;
 use App\Services\GeoIp\GeoIp;
 use App\Services\GeoIp\IpStack;
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,9 +19,15 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    // add () => UrlGenerator $url
+    public function boot(UrlGenerator $url)
     {
         Paginator::useBootstrap();
+
+        Schema::defaultStringLength(191);
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 
     /**
@@ -31,5 +39,8 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(GeoIp::class, IpStack::class);
         $this->app->register(\L5Swagger\L5SwaggerServiceProvider::class);
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
